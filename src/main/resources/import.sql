f0b06b049fe17db45f4998963063f198eaa8d1ce
JoaquinHerreraB <70816183+JoaquinHerreraB@users.noreply.github.com>
2021-03-23 13:14:59 -0500

    ultimas-imagenes-nuevas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,7 +13,7 @@ insert into `faztty`.`usuario` (tipo, id, email, password, ruc, nombre,direccion
 
 
 insert into `faztty`.`categoria` (id, nombre, descripcion) values (1,'Lacteo','Productos derivados de la leche');
-insert into `faztty`.`categoria` (id, nombre, descripcion) values (2,'Galleta','Galletas empaquetadas');
+insert into `faztty`.`categoria` (id, nombre, descripcion) values (2,'Galleta Dulce','Galletas empaquetadas de sabores dulces');
 insert into `faztty`.`categoria` (id, nombre, descripcion) values (3,'Snack','Snacks empaquetadas, pequeños o grandes');
 
 insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (1,'Tarro de Leche Gloria Azul','Gloria', 3.50, 'gloriaazul.jpg', 78,1,3 );
@@ -23,3 +23,10 @@ insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_c
 insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (4,'Tarro de Leche Gloria','Gloria', 3.50, 'gloriaazul.jpg', 78,1,4 );
 insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (5,'Leche Condensada','Gloria', 5.00, 'condensada.jpg', 89,1,4 );
 
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (6,'Galleta Picaras Clásicas 6 u','Winters', 3.00, 'picararoja.jpg', 87,2,3 );
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (7,'Galleta Picaras Extremas 6 u','Winters', 3.00, 'picaranegra.jpg', 89,2,3 );
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (8,'Paquete Doritos 210g','Frito-Lay', 8.40 ,'doritos.jpg', 78,3,3 );
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (9,'Paquete Cheetos 200g','Frito-Lay', 7.00 ,'chetos.jpg', 86,3,3 );
+
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (10,'Paquete Picaras Clásicas 6 u','Winters', 3.20, 'picararoja.jpg', 84,2,4 );
+insert into `faztty`.`producto` (id, nombre, marca,precio,imagen,puntuacion,id_categoria,id_negocio) values (11,'Paquete Cheetos Grande (200g)','Frito-Lay', 7.50 ,'chetos2.jpg', 67,3,4 );
